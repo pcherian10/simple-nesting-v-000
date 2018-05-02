bcbd69bd0f6a39ceccc8c27ff893e9b719c61b33
Paul <pcherian10@gmail.com>
2018-05-02 21:02:40 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -52,7 +52,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:known_for]
+		 programmer_hash[:dennis_ritchie][:known_for].to_s
 end
 
 def adding_matz
