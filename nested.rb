a8c84e23ba62d88d0c3d9d7818114b10d1558676
Paul <pcherian10@gmail.com>
2018-05-02 21:28:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,6 @@ def changing_alan
      alans_new_info = "GUI"
 	 programmer_hash[:alan_kay][:known_for] = alans_new_info
 	 programmer_hash
-
-
 end
 
 def adding_to_dennis
@@ -124,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
